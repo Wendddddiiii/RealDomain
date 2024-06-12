7cc05ed67e8e883f7134fc2c6547084b5979525b
Wendddddiiii <wenditang058@gmail.com>
2024-06-13 00:46:02 +1000

    complete delete user listing functionality

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -133,6 +133,24 @@ export default function Profile() {
     }
   };
 
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) => prev.filter((listing) => listing._id !== listingId));
+    } catch(error) {
+      console.log(error.message);
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7 text-yellow-800">Profile</h1>
@@ -215,7 +233,7 @@ export default function Profile() {
               </Link>
 
               <div className="flex flex-col items-center gap-3">
-                <button className="bg-yellow-300 text-yellow-900 rounded-lg p-3 hover:opacity-80 w-20">Delete</button>
+                <button onClick={() => handleListingDelete(listing._id)} className="bg-yellow-300 text-yellow-900 rounded-lg p-3 hover:opacity-80 w-20">Delete</button>
                 <button className="bg-yellow-400 text-yellow-900 rounded-lg p-3 hover:opacity-80 w-20">Edit</button>
               </div>
           </div>
