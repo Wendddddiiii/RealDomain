fdc34d67ee945be86a37eb37d997ed37ef40a214
Wendddddiiii <wenditang058@gmail.com>
2024-06-17 14:08:55 +1000

    add pre installation

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
 
-
+import path from 'path';
 
 dotenv.config();
 const app = express();
@@ -20,6 +20,8 @@ mongoose
     .catch((err) => {
         console.error('Error connecting to MongoDB:', err);
     });
+
+    const __dirname = path.resolve();
 
 // Define the port number
 const PORT = process.env.PORT || 3001;
@@ -35,6 +37,14 @@ app.listen(PORT, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, 'frontend', 'dist', 'index.html')));
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/frontend/dist/index.html'));
+})
+
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
