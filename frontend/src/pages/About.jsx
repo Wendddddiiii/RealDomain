792eb41a8f6b1664c5602db9484e3a1de01b0856
Wendddddiiii <wenditang058@gmail.com>
2024-06-17 14:00:37 +1000

    complete the overview page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,6 +2,16 @@
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className="py-24 px-4 max-w-xl  mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-yellow-900">
+        RealDomain Overview
+      </h1>
+      <p className="mb-5 text-yellow-700 ">
+      Are you ready to find your dream home, sell your property, or discover the perfect rental? Look no further! RealDomain is your comprehensive solution for all your real estate needs. As a leading agency specializing in buying, selling, and renting properties, we are dedicated to providing exceptional service and expertise to help you navigate the dynamic real estate market.
+      </p>
+      <p className="mb-5 text-yellow-700 ">
+      At RealDomain, we are committed to building lasting relationships with our clients based on trust, integrity, and exceptional service. Whether you are buying, selling, or renting, we are here to support you every step of the way, ensuring a smooth and successful real estate experience.
+      </p>
+    </div>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -110,8 +110,8 @@ export default function Home() {
         {saleListings && saleListings.length > 0 && (
           <div className=''>
             <div className='my-3'>
-              <h2 className='text-2xl font-semibold text-slate-600'>Recent places for sale</h2>
-              <Link className='text-sm text-blue-800 hover:underline' to={'/search?type=sale'}>Show more places for sale</Link>
+              <h2 className='text-2xl font-semibold text-amber-600'>Recent places for sale</h2>
+              <Link className='text-sm text-green-900 hover:underline' to={'/search?type=sale'}>Show more places for sale</Link>
             </div>
             <div className='flex flex-wrap gap-4'>
               {saleListings.map((listing) => (
