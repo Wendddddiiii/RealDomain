a333c398ab933aa8119540f90643d26ee0f3f9be
Wendddddiiii <wenditang058@gmail.com>
2024-06-17 20:34:03 +1000

    fix the sign-in page bug

--- frontend/src/components/OAuth.jsx
+++ frontend/src/components/OAuth.jsx
@@ -26,7 +26,7 @@ export default function OAuth() {
             });
             const data = await res.json();
             dispatch(signInSuccess(data));
-            navigate('/dashboard');
+            navigate('/');
         } catch (error) {
             console.log('Could not sign in with Google', error);
         } 
